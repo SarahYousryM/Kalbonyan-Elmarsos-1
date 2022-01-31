794537a53822f8567b7b529a00f5585fdba58d74
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 22:25:16 +0200

    done

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/Numbers.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/Numbers.js
@@ -1,1 +1,10 @@
-The Modern JavaScript Bootcamp
+let age = 20
+let dogYears = (age + 1) / 7
+
+console.log(dogYears)
+
+let studentScore = 41
+let maxScore = 100
+let percent = (studentScore / maxScore) * 100
+
+console.log(percent)

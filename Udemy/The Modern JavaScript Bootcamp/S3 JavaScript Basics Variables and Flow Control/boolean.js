63ed0d7b37c38bc00155e1af10898c990a3d2c15
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 22:26:48 +0200

    done

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/boolean.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/boolean.js
@@ -1,1 +1,21 @@
-The Modern JavaScript Bootcamp
+
+let temp = 180
+
+if (temp <= 32) {
+    console.log('It is freezing outside')
+}
+
+if (temp >= 110) {
+    console.log('It is way to hot outside')
+}
+
+
+let age = 6
+
+if (age <= 7) {
+    console.log('You will get a child discount')
+}
+
+if (age >= 65) {
+    console.log('You will get a senior discount')
+}

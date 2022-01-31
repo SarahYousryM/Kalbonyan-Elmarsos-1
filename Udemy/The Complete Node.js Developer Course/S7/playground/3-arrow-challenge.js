212cb94d7d8fb589576a77e597eb734e66d8d334
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 20:53:41 +0200

    done

--- Udemy/The Complete Node.js Developer Course/S7/playground/3-arrow-challenge.js
+++ Udemy/The Complete Node.js Developer Course/S7/playground/3-arrow-challenge.js
@@ -1,1 +1,17 @@
-The Complete Node.js Developer Course (3rd Edition)
+const tasks = {
+    tasks: [{
+        text: 'eat',
+        completed: true
+    }, {
+        text: 'drink',
+        completed: false
+    }, {
+        text: 'shhoping',
+        completed: false
+    }],
+    getTasksToDo() {
+        return this.tasks.filter((task) => task.completed === false)
+    }
+}
+
+console.log(tasks.getTasksToDo())

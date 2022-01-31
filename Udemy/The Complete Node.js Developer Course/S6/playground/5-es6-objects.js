4304ee78930603c123201b868705cb9ef71495da
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 19:07:42 +0200

    done

--- Udemy/The Complete Node.js Developer Course/S6/playground/5-es6-objects.js
+++ Udemy/The Complete Node.js Developer Course/S6/playground/5-es6-objects.js
@@ -1,1 +1,27 @@
-The Complete Node.js Developer Course (3rd Edition)
+const name = 'Andrew'
+const userAge = 27
+
+const user = {
+    name,
+    age: userAge,
+    location: 'Philadelphia'
+}
+
+console.log(user)
+
+
+const product = {
+    label: 'Red notebook',
+    price: 3,
+    stock: 201,
+    salePrice: undefined,
+    rating: 4.2
+}
+
+
+
+const transaction = (type, { label, stock }) => {
+    console.log(type, label, stock)
+}
+
+transaction('order', product)

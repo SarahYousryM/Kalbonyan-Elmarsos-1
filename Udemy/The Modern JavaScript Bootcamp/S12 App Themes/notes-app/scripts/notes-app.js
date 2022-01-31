df7cfdcb934cfba148ddf8cf986f2e58b69db724
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 23:50:43 +0200

    done

--- Udemy/The Modern JavaScript Bootcamp/S12 App Themes/notes-app/scripts/notes-app.js
+++ Udemy/The Modern JavaScript Bootcamp/S12 App Themes/notes-app/scripts/notes-app.js
@@ -1,1 +1,23 @@
-The Modern JavaScript Bootcamp
+let notes = getSavedNotes()
+
+const filters = {
+    searchText: '',
+    sortBy: 'byEdited'
+}
+
+renderNotes(notes, filters)
+
+document.querySelector('#create-note').addEventListener('click', (e) => {
+    const id = uuidv4()
+    const timestamp = moment().valueOf()
+
+    notes.push({
+        id: id,
+        title: '',
+        body: '',
+        createdAt: timestamp,
+        updatedAt: timestamp
+    })
+    saveNotes(notes)
+    location.assign(`/edit.html#${id}`)
+})

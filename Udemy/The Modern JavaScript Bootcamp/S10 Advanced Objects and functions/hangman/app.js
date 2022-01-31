af575ce1c9b0da785d51fa3fff665ca0fb0185cb
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 23:40:59 +0200

    done

--- Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/hangman/app.js
+++ Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/hangman/app.js
@@ -1,1 +1,8 @@
-The Modern JavaScript Bootcamp
+let gettep = function (total, Percentage = .2) {
+    let percent = Percentage * 100
+    let tep = total * Percentage
+    return `A ${percent}% tep on $${total} would be $${tep}`
+}
+
+let tep = gettep(60)
+console.log(tep)

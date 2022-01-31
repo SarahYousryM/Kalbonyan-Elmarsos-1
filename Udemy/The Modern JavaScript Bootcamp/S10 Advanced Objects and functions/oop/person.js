64074fc619dfaed9554cfe80b430a5551f2a46fa
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 23:44:01 +0200

    done

--- Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/oop/person.js
+++ Udemy/The Modern JavaScript Bootcamp/S10 Advanced Objects and functions/oop/person.js
@@ -1,1 +1,16 @@
-The Modern JavaScript Bootcamp
+class Person {
+    constructor(firstName, lastName, age, likes = []) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.age = age
+        this.likes = likes
+    }
+    getBio() {
+        let bio = `${this.firstName} is ${this.age}.`
+
+        this.likes.forEach((like) => {
+            bio += ` ${this.firstName} likes ${like}.`
+        })
+
+        return bio
+    }

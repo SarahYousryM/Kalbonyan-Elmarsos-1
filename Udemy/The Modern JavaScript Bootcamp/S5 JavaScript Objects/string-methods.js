6c296645e4269552e2cd9a40ad041cb2a0bcf437
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 23:09:39 +0200

    done

--- Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/string-methods.js
+++ Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/string-methods.js
@@ -1,1 +1,7 @@
-The Modern JavaScript Bootcamp
+let isValidPassword = function (password) {
+    return password.length > 8 && !password.includes('password')
+}
+
+console.log(isValidPassword('qwer'))
+console.log(isValidPassword('jdkljls'))
+console.log(isValidPassword('qwerty'))

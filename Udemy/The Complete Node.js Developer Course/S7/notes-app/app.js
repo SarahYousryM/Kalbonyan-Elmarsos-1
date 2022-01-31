55e99b8f2e9bf7c7274f0e7da5c0b9db9fc24f49
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 19:17:48 +0200

    done

--- Udemy/The Complete Node.js Developer Course/S7/notes-app/app.js
+++ Udemy/The Complete Node.js Developer Course/S7/notes-app/app.js
@@ -1,1 +1,8 @@
-The Complete Node.js Developer Course (3rd Edition)
+const chalk = require('chalk')
+const getNotes = require('./notes.js')
+const msg = getNotes()
+console.log(msg)
+const greenmsg = chalk.blue.inverse.bold('succes')
+console.log(greenmsg)
+
+console.log(process.argv[2])

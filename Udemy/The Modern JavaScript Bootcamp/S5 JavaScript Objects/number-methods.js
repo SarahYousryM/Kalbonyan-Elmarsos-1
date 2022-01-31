77971fb8bce90aa724632b3a6c395c6b75ab0c3e
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 23:04:20 +0200

    done

--- Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/number-methods.js
+++ Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/number-methods.js
@@ -1,1 +1,12 @@
-The Modern JavaScript Bootcamp
+let num = 103.941
+
+console.log(num.toFixed(2))
+
+console.log(Math.round(num))
+console.log(Math.floor(num))
+console.log(Math.ceil(num))
+
+let min = 0
+let max = 1
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+

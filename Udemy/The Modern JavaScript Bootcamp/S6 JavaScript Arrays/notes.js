526d8a31334a0200957dc672620df56b441b36ac
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 23:19:36 +0200

    done

--- Udemy/The Modern JavaScript Bootcamp/S6 JavaScript Arrays/notes.js
+++ Udemy/The Modern JavaScript Bootcamp/S6 JavaScript Arrays/notes.js
@@ -1,1 +1,22 @@
-The Modern JavaScript Bootcamp
+const notes = [{
+    title: 'my next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
+
+const sortNotes = function (notes) {
+    notes.sort(function (a, b) {
+        if (a.title.toLowerCase() < b.title.toLowerCase()) {
+            return -1
+        } else if (b.title.toLowerCase() < a.title.toLowerCase()) {
+            return 1
+        } else {
+            return 0
+        }
+   })
+}

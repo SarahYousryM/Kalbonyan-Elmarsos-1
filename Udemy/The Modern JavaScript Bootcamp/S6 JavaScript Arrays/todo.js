80a65196cec415fcbe1cfc6af8921c0f77afe9ef
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 23:20:35 +0200

    done

--- Udemy/The Modern JavaScript Bootcamp/S6 JavaScript Arrays/todo.js
+++ Udemy/The Modern JavaScript Bootcamp/S6 JavaScript Arrays/todo.js
@@ -1,1 +1,27 @@
-The Modern JavaScript Bootcamp
+const todos = [{
+    text: 'Order cat food',
+    completed: false
+}, {
+    text: 'Clean kitchen',
+    completed: true
+}, {
+    text: 'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed: false
+}, {
+    text: 'Exercise',
+    completed: true}]
+
+const sortTodos = function (todos) {
+    todos.sort(function (a, b) {
+        if (!a.completed && b.completed) {
+            return -1
+        } else if (!b.completed && a.completed) {
+            return 1
+        } else {
+            return 0
+      }
+  })
+}

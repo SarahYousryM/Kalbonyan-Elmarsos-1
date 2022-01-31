ae5eebfb70b40ba66e37d49c4ce3231d32640a33
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 23:51:57 +0200

    done

--- Udemy/The Modern JavaScript Bootcamp/S12 App Themes/notes-app/scripts/notes-functions.js
+++ Udemy/The Modern JavaScript Bootcamp/S12 App Themes/notes-app/scripts/notes-functions.js
@@ -1,1 +1,9 @@
-The Modern JavaScript Bootcamp
+const getSavedNotes = () => {
+    const notesJSON = localStorage.getItem('notes')
+
+    try {
+        return notesJSON ? JSON.parse(notesJSON) : []
+    } catch (e) {
+        return []
+    } 
+}

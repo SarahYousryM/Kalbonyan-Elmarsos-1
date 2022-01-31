d02d88a1d183df2b57b38f5b2721f99d0c06a091
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 23:06:45 +0200

    done

--- Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/objects-functions.js
+++ Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/objects-functions.js
@@ -1,1 +1,10 @@
-The Modern JavaScript Bootcamp
+let convertFahrenheit = function (fahrenheit) {
+    return {
+        fahrenheit: fahrenheit,
+        kelvin: (fahrenheit + 459.67) * (5 / 9),
+        celsius: (fahrenheit - 32) * (5 / 9)
+    }
+}
+
+let temps = convertFahrenheit(74)
+console.log(temps)

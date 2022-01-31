0eeec8253f3a83ea9b25aeb16a40fc532ba1137a
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 22:33:37 +0200

    done

--- Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/temp-conversion.js
+++ Udemy/The Modern JavaScript Bootcamp/S3 JavaScript Basics Variables and Flow Control/temp-conversion.js
@@ -1,1 +1,6 @@
-The Modern JavaScript Bootcamp
+let fahrenheit = 50 
+let celsius = (fahrenheit - 32) * 5 / 9
+let kelvin = (fahrenheit + 459.67) * 5 / 9
+
+console.log(celsius)
+console.log(kelvin)

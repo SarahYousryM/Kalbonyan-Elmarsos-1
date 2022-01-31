709d253e47f20b38ce4a33c6fa942138348ea376
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 21:25:22 +0200

    done

--- Udemy/The Complete Node.js Developer Course/S10/task-manager/mongodb.js
+++ Udemy/The Complete Node.js Developer Course/S10/task-manager/mongodb.js
@@ -1,1 +1,11 @@
-The Complete Node.js Developer Course (3rd Edition)
+const { MongoClient, ObjectID } = require('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!')
+    }
+
+    const db = client.db(databaseName)

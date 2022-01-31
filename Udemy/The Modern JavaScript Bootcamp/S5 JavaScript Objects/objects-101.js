50c7dd6af3acd1bb4365e5a28f6ce96f535a7f16
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 23:05:45 +0200

    done

--- Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/objects-101.js
+++ Udemy/The Modern JavaScript Bootcamp/S5 JavaScript Objects/objects-101.js
@@ -1,1 +1,9 @@
-The Modern JavaScript Bootcamp
+let me = {
+    name: 'sarah',
+    age: 20,
+    location: 'egypt'
+}
+
+console.log(`${me.name} is ${me.age} and lives in ${me.location}.`)
+me.age = me.age + 1
+console.log(`${me.name} is ${me.age} and lives in ${me.location}.`)

297a8c007d9fd9615ccea67a30763f64edc8702b
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 18:23:46 +0200

    done

--- Udemy/The Complete Node.js Developer Course/S4/playground/2-arrow-function.js
+++ Udemy/The Complete Node.js Developer Course/S4/playground/2-arrow-function.js
@@ -1,1 +1,11 @@
-The Complete Node.js Developer Course (3rd Edition)
+const event = {
+    name: 'Birthday Party',
+    guestList: ['Andrew', 'Jen', 'Mike'],
+    printGuestList() {
+        console.log('Guest list for ' + this.name)
+        
+        this.guestList.forEach((guest) => {
+            console.log(guest + ' is attending ' + this.name)
+        })
+    }
+}

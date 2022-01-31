9a24f5adfb183e01600c7b4b54ea32d00dbf938b
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 02:56:51 +0200

    done

--- Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
+++ Linkedin-Learning/Programming-Foundation-Fundamentals/Challengs/Favorite-Cities-Challenge.py
@@ -1,1 +1,7 @@
 
+def favorite_city(name):
+print("one of my favorite cities is", name)
+
+favorite_city("Santa Barbara, California")
+favorite_city("Asheville, North Carolina")
+favorite_city("Amsterdam, The Netherlands")

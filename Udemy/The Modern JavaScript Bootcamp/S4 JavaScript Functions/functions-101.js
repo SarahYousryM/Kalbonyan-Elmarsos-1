a771bb0439c7f1d5b98cbc674f96570a1d760187
SarahYousryM <93348321+SarahYousryM@users.noreply.github.com>
2022-01-31 22:38:28 +0200

    done

--- Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/functions-101.js
+++ Udemy/The Modern JavaScript Bootcamp/S4 JavaScript Functions/functions-101.js
@@ -1,1 +1,18 @@
-The Modern JavaScript Bootcamp
+let greetUser = function () {
+    console.log('Welcome user!')
+}
+
+greetUser()
+greetUser()
+greetUser()
+
+let square = function (num) {
+    let result = num * num
+    return result
+}
+
+let value = square(3)
+let otherValue = square(10)
+
+console.log(value)
+console.log(otherValue)
